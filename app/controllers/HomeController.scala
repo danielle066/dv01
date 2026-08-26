1121eab324da279f0ffd87d69c52c10210a06235
Danielle <hello.danielle.us@gmail.com>
2026-08-26 12:09:27 -0400

    feat: create loan stats visualization UI

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,11 +1,29 @@
 package controllers
 
 import javax.inject._
+
 import play.api.mvc._
 
+import scala.io.Source
+import scala.util.control.NonFatal
+
 @Singleton
-class HomeController @Inject(cc: ControllerComponents) extends AbstractController(cc) {
+class HomeController @Inject()(cc: ControllerComponents, env: play.api.Environment)
+  extends AbstractController(cc) {
+
   def index: Action[AnyContent] = Action {
-    Ok("DV01 loan stats")
+    val file = env.getFile("public/index.html")
+    if (!file.exists()) {
+      NotFound("public/index.html not found")
+    } else {
+      val source = Source.fromFile(file)(scala.io.Codec.UTF8)
+      try {
+        Ok(source.mkString).as("text/html")
+      } catch {
+        case NonFatal(e) => InternalServerError(e.getMessage)
+      } finally {
+        source.close()
+      }
+    }
   }
 }
